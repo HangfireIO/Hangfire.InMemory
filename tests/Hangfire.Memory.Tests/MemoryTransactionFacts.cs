dab1a54acbbc0e78cc8a90b9adbe2a722d29b522
Sergey Odinokov <odinserj@gmail.com>
2020-09-04 11:20:37 +0300

    Add first unit test for MemoryTransaction

---
+++ tests/Hangfire.Memory.Tests/MemoryTransactionFacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Hangfire.Memory.Tests
+{
+    public class MemoryTransactionFacts
+    {
+        [Fact]
+        public void Ctor_ThrowsAnException_WhenDispatcherIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new MemoryTransaction(null));
+            Assert.Equal("dispatcher", exception.ParamName);
+        }
+    }
+}
