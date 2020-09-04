9e37a268bd16dd37a87d077595582d6713c4b18d
Sergey Odinokov <odinserj@gmail.com>
2020-09-04 10:30:04 +0300

    Use parallel loop for HarnessHostedService

---
+++ samples/ConsoleSample/HarnessHostedService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleSample
+{
+    public class HarnessHostedService : BackgroundService
+    {
+        private readonly IBackgroundJobClient _backgroundJobs;
+        private readonly ILogger<HarnessHostedService> _logger;
+
+        public HarnessHostedService(IBackgroundJobClient backgroundJobs, ILogger<HarnessHostedService> logger)
+        {
+            _backgroundJobs = backgroundJobs ?? throw new ArgumentNullException(nameof(backgroundJobs));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var sw = Stopwatch.StartNew();
+
+            Parallel.For(0, 500_000, i =>
+            {
+                _backgroundJobs.Enqueue(() => Empty());
+            });
+
+            _logger.LogInformation($"Enqueued in {sw.Elapsed}");
+            return Task.CompletedTask;
+        }
+
+        public static void Empty()
+        {
+        }
+    }
+}
--- samples/ConsoleSample/Startup.cs
+++ samples/ConsoleSample/Startup.cs
@@ -1,46 +1,11 @@
-using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ConsoleSample
 {
-    public class HarnessHostedService : BackgroundService
-    {
-        private readonly IBackgroundJobClient _backgroundJobs;
-        private readonly ILogger<HarnessHostedService> _logger;
-
-        public HarnessHostedService(IBackgroundJobClient backgroundJobs, ILogger<HarnessHostedService> logger)
-        {
-            _backgroundJobs = backgroundJobs ?? throw new ArgumentNullException(nameof(backgroundJobs));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
-        
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            var sw = Stopwatch.StartNew();
-
-            for (var i = 0; i < 500_000; i++)
-            {
-                _backgroundJobs.Enqueue(() => Empty());
-            }
-
-            _logger.LogInformation($"Enqueued in {sw.Elapsed}");
-
-            return Task.CompletedTask;
-        }
-
-        public static void Empty()
-        {
-        }
-    }
-
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
