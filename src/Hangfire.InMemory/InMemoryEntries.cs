a49477f0227810b7ca8b2e52ddab2ebc7761db87
Sergey Odinokov <odinserj@gmail.com>
2023-07-24 16:46:33 +0700

    Remove unreachable code for struct

--- src/Hangfire.InMemory/InMemoryEntries.cs
+++ src/Hangfire.InMemory/InMemoryEntries.cs
@@ -316,10 +316,6 @@ namespace Hangfire.InMemory
 
         public int Compare(SortedSetEntry x, SortedSetEntry y)
         {
-            if (ReferenceEquals(x, y)) return 0;
-            if (ReferenceEquals(null, y)) return 1;
-            if (ReferenceEquals(null, x)) return -1;
-
             var scoreComparison = x.Score.CompareTo(y.Score);
             if (scoreComparison != 0 ||
                 ReferenceEquals(null, y.Value) ||
